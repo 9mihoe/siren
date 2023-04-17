2b82f518740f3f1611b58aa7011530d87431ef76
9mihoe <mystery.gw@gmail.com>
2023-04-17 23:57:43 +0800

    wip

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 mod player;
+// use rand::Rng; // 0.8.5
 
 fn main() {
-    // Try: https://stackoverflow.com/questions/67656408/how-to-properly-initialize-a-vector-in-rust-given-that-the-vector-will-be-immut
-    let mut players: Vec<player::Player> = Vec::new();
-    let NUM_PLAYERS: u32 = 100;
-    for i in 0..NUM_PLAYERS {
-        players.push(player::Player::new(0.1, 0.1));
-    }
-    // let p = player::Player::new(0.1, 0.1);
-    // let will_jump = p.will_jump();
-    println!("hello world");
+    let num_players: u32 = 100;
+    let players: Vec<player::Player> = {
+        let mut tmp: Vec<player::Player> = Vec::new();
+        for _ in 0..num_players {
+            tmp.push(player::Player::new(0.1, 0.1));
+        }
+        tmp
+    };
+    println!("{}", players.len());
 }
--- src/player.rs
+++ src/player.rs
@@ -4,6 +4,14 @@ pub struct Player {
     aggression: f64
 }
 
+enum CharacterCategory {
+    VHi,
+    Hi,
+    Mid,
+    Lo,
+    VLo
+}
+
 impl Player {
     pub fn new(cooperation_: f64, aggression_: f64) -> Player {
         Player{cooperation: cooperation_, aggression: aggression_}
@@ -15,4 +23,17 @@ impl Player {
         }
         return false;
     }
+
+    // https://stackoverflow.com/questions/49037111/alternatives-to-matching-floating-point-ranges
+    fn get_category(character: f64) -> CharacterCategory {
+        return match character {
+            character if character>0.8 => CharacterCategory::VHi,
+            character if character>0.6 => CharacterCategory::Hi,
+            character if character>0.4 => CharacterCategory::Mid,
+            character if character>0.2 => CharacterCategory::Lo,
+            character if character>0.0 => CharacterCategory::VLo,
+            _ => panic!("Character trait out-of-bounds!")
+        };
+    }
+
 }
